df9af855bbaa42154305da9f981a3ed29a91282d
Diego_Souza <diego.s.costa@fiesc.com.br>
2021-04-29 12:02:42 -0300

    Alterada coloraçao do background com js quando nao atendida as condiçoes propostas!

--- js/first.js
+++ js/first.js
@@ -16,11 +16,13 @@ for (var i = 0; i < pacientes.length; i++) {
 
     if (peso <= 0 || peso >= 1000) {
         pesoValido = false;
+        paciente.style.backgroundColor = "lightcoral";
         tdImc.textContent = "Peso Inválido!";
     }
 
     if (altura <= 0 || altura >= 3.00) {
         alturaValida = false;
+        paciente.style.backgroundColor = "lightcoral";
         tdImc.textContent = "Altura Inválida!"
     }
 
