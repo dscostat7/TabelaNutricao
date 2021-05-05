2bfe4b6f889e28dee6a46a13bb4d68b6bc37d945
Diego_Souza <diego.s.costa@fiesc.com.br>
2021-05-05 10:25:56 -0300

    ajustado mostragem de erros quando a requisiçao nao funcionar corretamente

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -3,13 +3,22 @@ var botaoAdicionar = document.querySelector("#buscar-pacientes");
 botaoAdicionar.addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+    var erroAjax = document.querySelector("#erro-ajax");
     xhr.addEventListener("load", function () {
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta); // transformando de JSON para JS
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta); // transformando de JSON para JS
 
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });            
+        } else {
+            alert(xhr.status);
+            alert(xhr.responseText);
+            erroAjax.classList.remove("invisivel");
+        }
+
     })
     xhr.send();
 })
