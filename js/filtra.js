46d60e67865bdf59441ea194dd2ecaf2510d94bd
Diego_Souza <diego.s.costa@fiesc.com.br>
2021-05-05 09:14:55 -0300

    Implementaçao campo de buscas com expressao regular!

--- js/filtra.js
+++ js/filtra.js
@@ -1,11 +1,26 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function () {
-    var paciente = document.querySelectorAll(".paciente");
+    var pacientes = document.querySelectorAll(".paciente");
 
-    for (var i = 0; i < paciente.length; i++) {
-        var paciente = pacientes[i];
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = paciente.textContent;
+    if (this.value.length > 0) {
+       for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
+
+            if (!expressao.test(nome)) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        } 
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
     }
+    
 })
